1919c1b450c05c348183e0536f41fcdf3216c45b
Stefano Montanelli <stefano.montanelli@unimi.it>
2022-04-14 13:20:23 +0200

    Added publicpath configuration

---
+++ Front-end/ci-meta/vue.config.js
@@ -0,0 +1,15 @@
+// vue.config.js
+
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
+//module.exports = {
+//	publicPath: '/cimeta/'
+//}
+
+// vue.config.js
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
+  publicPath: '/cimeta'
+})
